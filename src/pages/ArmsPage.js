9901a0484c37db72be11c5fe8719057a4d5f3510
Changh2 <changhi9701@gmail.com>
2024-06-29 00:42:22 +0900

    feat: Arms 페이지 구현

--- src/pages/ArmsPage.js
+++ src/pages/ArmsPage.js
@@ -1,24 +1,44 @@
 import React from 'react';
+import './ArmsPage.css'; // CSS 파일 임포트
 
 const ArmsPage = () => {
   return (
-    <div>
-      <h1>팔 운동</h1>
-      <ul>
-        <li>
-          <a href="https://www.youtube.com/watch?v=example1" target="_blank" rel="noopener noreferrer">
-            팔 운동 1
-          </a>
-          <p>팔 운동 1에 대한 설명입니다.</p>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/watch?v=example2" target="_blank" rel="noopener noreferrer">
-            팔 운동 2
-          </a>
-          <p>팔 운동 2에 대한 설명입니다.</p>
-        </li>
-        {/* 추가 운동 링크와 설명 */}
-      </ul>
+    <div className="arms-page-container">
+      <div className="image-container">
+        <img src="/images/armsPelican.jpeg" alt="Arms Exercise" className="exercise-image" />
+      </div>
+      <div className="content-container">
+        <div className="video-container">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/ZDIIg4kvY-c?si=dw4ebCA0Q1cSuhlR" 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen>
+          </iframe>
+        </div>
+        <div className="description-container">
+          <p>운동에 대한 설명입니다. 이곳에 운동에 대한 상세 설명을 작성하세요.</p>
+        </div>
+      </div>
+      <div className="content-container">
+        <div className="video-container">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/JsF2nzetLBU?si=lh2Zv_6sQesCZ9kq" 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen>
+          </iframe>
+        </div>
+        <div className="description-container">
+          <p>운동에 대한 설명입니다. 이곳에 운동에 대한 상세 설명을 작성하세요.</p>
+        </div>
+      </div>
     </div>
   );
 };
