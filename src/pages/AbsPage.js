4760c04df60010e6440d80d76c2aa266e6dd162e
Changh2 <changhi9701@gmail.com>
2024-06-29 04:45:39 +0900

    chore: 수정

--- src/pages/AbsPage.js
+++ src/pages/AbsPage.js
@@ -12,7 +12,7 @@ const AbsPage = () => {
           <iframe 
             width="560" 
             height="315" 
-            src="https://www.youtube.com/embed/video-id-1" 
+            src="https://www.youtube.com/embed/QSZ0mUGO_CA?si=ZRS6X5Giq-VqPSYc"
             title="YouTube video player" 
             frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
@@ -28,8 +28,38 @@ const AbsPage = () => {
           <iframe 
             width="560" 
             height="315" 
-            src="https://www.youtube.com/embed/video-id-2" 
+            src="https://www.youtube.com/embed/mVsORhCxNL0?si=15s-z-cZwxg1N2ip"
             title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen>
+          </iframe>
+        </div>
+        <div className="description-container">
+          <p>운동에 대한 설명입니다. 이곳에 운동에 대한 상세 설명을 작성하세요.</p>
+        </div>
+      </div>
+      <div className="content-container">
+        <div className="video-container">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/2G_P1Y9UG0Q?si=-RW0kgM2ghXnal-A"
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen>
+          </iframe>
+        </div>
+        <div className="description-container">
+          <p>운동에 대한 설명입니다. 이곳에 운동에 대한 상세 설명을 작성하세요.</p>
+        </div>
+      </div>
+      <div className="content-container">
+        <div className="video-container">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/3JaMsw1cqcU?si=JF_X62qTV5N3pvoj"
             frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
             allowFullScreen>
--- src/pages/ArmsPage.js
+++ src/pages/ArmsPage.js
@@ -39,6 +39,35 @@ const ArmsPage = () => {
           <p>운동에 대한 설명입니다. 이곳에 운동에 대한 상세 설명을 작성하세요.</p>
         </div>
       </div>
+      <div className="content-container">
+        <div className="video-container">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/R2-QUEff8NY?si=DIzSkRFfhNDeLExG"
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen>
+          </iframe>
+        </div>
+        <div className="description-container">
+          <p>운동에 대한 설명입니다. 이곳에 운동에 대한 상세 설명을 작성하세요.</p>
+        </div>
+      </div>
+      <div className="content-container">
+        <div className="video-container">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/eRpPqnmb0PM?si=kCtf0dT8VNslgsYF"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen>
+          </iframe>
+        </div>
+        <div className="description-container">
+          <p>운동에 대한 설명입니다. 이곳에 운동에 대한 상세 설명을 작성하세요.</p>
+        </div>
+      </div>
     </div>
   );
 };
