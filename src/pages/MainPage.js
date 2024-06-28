fc4f8fb3a416fbbc1b213b5bab33a5826c901d4b
Changh2 <changhi9701@gmail.com>
2024-06-28 20:53:36 +0900

    feat: nav바 구현

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,10 +5,6 @@ import './MainPage.css'; // CSS 파일
 const MainPage = () => {
   return (
     <div className="main-page">
-      <nav className="navbar">
-        <Link to="/signup" className="signup-link">Sign Up</Link> {/* Sign Up 링크 추가 */}
-        <Link to="/signin" className="signip-link">Sign In</Link> {/* Sign In 링크 추가 */}
-      </nav>
       <div className="body-part">
         <img src="human.png" alt="Human Body" className="body-image" />
         <Link to="/arms" className="arm-link">팔</Link>
